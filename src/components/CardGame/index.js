46b361689efbc8f2fad33bdf0aae8ef07b00d834
Leonardoluis22 <leonardofogaca67@gmail.com>
2022-07-12 20:32:57 -0400

    :tada: Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+//jsx
+import './style.css'; //importanto o arquivo css desse componente
+
+function CardGame() {
+  return `
+      <article class='card-game'>
+          <img src="images/alura-pixel.png" alt='Logo da Alura'>
+      </article>
+    `;
+}
+export default CardGame;
