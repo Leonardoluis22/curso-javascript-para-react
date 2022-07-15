209f477c6b56773633fc2bd77e3019d7f23b496f
Leonardoluis22 <leonardofogaca67@gmail.com>
2022-07-15 01:53:09 -0400

    :sparkles: news features

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -3,12 +3,12 @@ import '../CardFrontBack/style.css';
 function CardFrontBack() {
   return /*html*/ `
     <article class='card-front-back'>
-    ${CardGame()}
-    ${CardGame(
-      './images/logo-javascript.png',
-      'Logo JavaScript',
-      'card-front-back'
-    )}
+   <div class='card front'>
+        ${CardGame()}
+   </div>
+   <div class='card -back'>
+        ${CardGame('./images/logo-javascript.png', 'Logo JavaScript')}
+    </div>
     </article>
 
   `;
